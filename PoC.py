9804c893f25dd08823109b65e3be2ba9f48bf8e7
Red Meow <44439821+aifred0729-TW@users.noreply.github.com>
2023-01-20 21:48:16 +0800

    Update PoC.py
    Add function of snapshot ID finder

--- PoC.py
+++ PoC.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # PoC By Red Meow
 # https://www.facebook.com/aifred0729TW/
-import requests, urllib3, base64
+import requests, urllib3, base64, json
 from urllib.parse import unquote
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -11,7 +11,13 @@ vcenter_password = "!Meowmeow.local8787"
 
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"}
 
+proxies = {
+   'http': 'http://127.0.0.1:8080',
+   'https': 'http://127.0.0.1:8080',
+}
+
 def pre_auth(base_url, headers):
+
     print("[*] Getting pre authentication cookie....")
     print("[*] Try URL : " + base_url + "/ui/config/h5-config?debug=false")
 
@@ -34,6 +40,7 @@ def pre_auth(base_url, headers):
     return pre_vsphere_ui_jsessionid
 
 def redirect(base_url, pre_vsphere_ui_jsessionid, headers):
+
     print("[*] Getting the redirect URL....")
     print("[*] Try URL : " + base_url + "/ui/login")
 
@@ -185,6 +192,7 @@ def xsrf_token(base_url, vsphere_username, vsphere_client_session_index, vsphere
     return vsphere_ui_xsrf_token
 
 def ret_cookies(vsphere_ui_jsessionid, vsphere_username, vsphere_client_session_index, vsphere_ui_xsrf_token, castle_session):
+
     vsphere_ui_jsessionid = {"VSPHERE-UI-JSESSIONID": vsphere_ui_jsessionid}
     vsphere_username = {"VSPHERE-USERNAME": vsphere_username}
     vsphere_client_session_index = {"VSPHERE-CLIENT-SESSION-INDEX": vsphere_client_session_index}
@@ -200,24 +208,25 @@ def ret_cookies(vsphere_ui_jsessionid, vsphere_username, vsphere_client_session_
 
     return cookies
 
-def revert(machine, base_url, cookies, headers):
+def revert(machine, snapshot_data, base_url, cookies, headers):
 
-    # DC01 3001 DC02 3002 WS01 3004 WS02 3006
-    id = ""
+    snapshot = []
+    machine_id = []
+    count = 0
+    find = False
+    for i in snapshot_data:
+        machine_id.append([])
+        machine_id[count].append(i)
+        machine_id[count].append(len(snapshot_data[i]))
+        count += 1
+    for i in range(len(machine_id)):
+        for j in range(machine_id[i][1]):
+            if machine == snapshot_data[machine_id[i][0]][j][1]:
+                serverGuid = snapshot_data[machine_id[i][0]][j][3]
+                find = True
 
-    if machine == "DC01":
-        id = "3001"
-    elif machine == "DC02":
-        id = "3002"
-    elif machine == "WS01":
-        id = "3004"
-    elif machine == "WS02":
-        id = "3006"
-    else:
-         print("[*] No Select Machine in Database.")
-
-    if id:
-        url = f"/ui/mutation/apply/urn%3Avmomi%3AVirtualMachineSnapshot%3Asnapshot-{id}%3Afc690f35-e97e-46d5-b3ae-5da238a51518?propertyObjectType=com.vmware.vsphere.client.vm.snapshot.VmSnapshotRevertSpec"
+    if find:
+        url = f"/ui/mutation/apply/urn:vmomi:VirtualMachineSnapshot:{machine}:{serverGuid}?propertyObjectType=com.vmware.vsphere.client.vm.snapshot.VmSnapshotRevertSpec"
         xsrf_header = {"X-Vsphere-Ui-Xsrf-Token": cookies["VSPHERE-UI-XSRF-TOKEN"]}
         te = {"Te": "trailers"}
         headers.update(xsrf_header)
@@ -235,19 +244,172 @@ def revert(machine, base_url, cookies, headers):
             print("[*] Selected machine will be revert now.")
         else:
             print("[!] Status code is not 200.")
+            exit()
+    else:
+         print("[*] No Select Machine in Database.")
 
-def main(base_url, vcenter_username, vcenter_password, headers):
+
+def get_api_session(base_url, vcenter_username, vcenter_password):
+
+    print("[*] Getting the API session....")
+    print("[*] Username : " + vcenter_username)
+    print("[*] Password : " + vcenter_password)
+
+    credential = vcenter_username + ":" + vcenter_password
+    credential = base64.b64encode(credential.encode()).decode()
+
+    auth_header = {"authorization": "Basic " + credential}
+
+    try:
+        r = requests.post(base_url + "/api/session", headers=auth_header, verify=False)
+    except:
+        print("[!] Cannot connect vCenter !!")
+        print("[!] Failed on get_api_session stage.")
+        exit()
+
+    if r.status_code == 201:
+        print("[*] Successful create API session.")
+        print("[$] API session : " + r.text.strip('"'))
+        result = r.text.strip('"')
+    else:
+        print("[!] Failed to create API session.")
+        print("[!] Failed on get_api_session stage.")
+        exit()
+
+    return result
+
+def get_machine_list(base_url, api_session):
+
+    print("[*] Getting the machine list....")
+
+    try:
+        r = requests.get(base_url + "/api/vcenter/vm", headers=api_session, verify=False)
+    except:
+        print("[!] Cannot connect vCenter !!")
+        print("[!] Failed on get_machine_list stage.")
+        exit()
+
+    if r.status_code == 200:
+        print("[*] Successfal get machine list.")
+        result = json.loads(r.text)
+    else:
+        print("[!] Failed to get machine list.")
+        print("[!] Failed on get_machine_list stage.")
+        exit()
+
+    return result
+
+def get_snapshot_id(base_url, machine_list, cookies):
+
+    print("[*] Getting snapshot id....")
+
+    machines = {}
+    snapshot_data = {}
+
+    for i in range(len(machine_list)):
+        tmp_machine_name = machine_list[i]['name']
+        tmp_machine_id = machine_list[i]['vm']
+        tmp_machine = {tmp_machine_name: tmp_machine_id}
+        machines.update(tmp_machine)
+
+    for i in machines:
+        try:
+            r = requests.get(base_url + f"/ui/data/properties/urn:vmomi:VirtualMachine:{machines[i]}:fc690f35-e97e-46d5-b3ae-5da238a51518?properties=name,snapshot,diskUsage", cookies=cookies, headers=headers, verify=False)
+        except:
+            print("[!] Cannot connect vCenter !!")
+            print("[!] Failed on get_snapshot_id stage.")
+            exit()
+
+        result = json.loads(r.text)
+        tmp_snapshots = {}
+        tmp_snapshots = snapshot_sort(tmp_snapshots, result, result, 0, 0, 0)
+        snapshot_data.update(tmp_snapshots)
+
+    return snapshot_data
+
+def snapshot_sort(snapshot_dict, original_data, result, root_count, child_count, count):
+    
+    data = []
+
+    if result['snapshot'] == None:
+        return snapshot_dict
+
+    elif "childSnapshotList" in result:
+
+        machine_name = original_data['name']
+        try:
+            for i in snapshot_dict[machine_name]:
+                data.append(i)
+            data.append([])
+            data[count].append(result['childSnapshotList'][child_count]['name'])
+            data[count].append(result['childSnapshotList'][child_count]['snapshot']['value'])
+            data[count].append(result['childSnapshotList'][child_count]['vm']['value'])
+            data[count].append(result['childSnapshotList'][child_count]['snapshot']['serverGuid'])
+            tmp_snapshots = {machine_name : data}
+            snapshot_dict.update(tmp_snapshots)
+            child_count += 1
+            count += 1
+            snapshot_sort(snapshot_dict, original_data, result, root_count, child_count, count)
+            return snapshot_dict
+        except:
+            return snapshot_dict
+
+    elif "rootSnapshotList" in result['snapshot']:
+        machine_name = result['name']
+        data.append([])
+        data[count].append(original_data['snapshot']['rootSnapshotList'][root_count]['name'])
+        data[count].append(original_data['snapshot']['rootSnapshotList'][root_count]['snapshot']['value'])
+        data[count].append(original_data['snapshot']['rootSnapshotList'][root_count]['vm']['value'])
+        data[count].append(original_data['snapshot']['rootSnapshotList'][root_count]['snapshot']['serverGuid'])
+        tmp_snapshots = {machine_name : data}
+        snapshot_dict.update(tmp_snapshots)
+        count += 1
+
+        if "childSnapshotList" in original_data['snapshot']['rootSnapshotList'][root_count] and original_data['snapshot']['rootSnapshotList'][root_count]['childSnapshotList'] != None:
+            snapshot_sort(snapshot_dict, original_data, result['snapshot']['rootSnapshotList'][root_count], root_count, child_count, count)
+            root_count += 1
+            return snapshot_dict
+        else:
+            return snapshot_dict
+    else:
+        return snapshot_dict
+
+def show_snapshots(snapshot_data):
+    
+    print("[*] Available Machine : ", end="")
+    machine_id = []
+    count = 0
+    for i in snapshot_data:
+        print(i, end=" ")
+        machine_id.append([])
+        machine_id[count].append(i)
+        machine_id[count].append(len(snapshot_data[i]))
+        count += 1
+    print()
+    print("   Machine Name\tSnapshot Name\tSnapshot ID\tVM ID  \tserverGuid")
+    print("   {:<12}\t{:<13}\t{:<13}\t{:<7}\t{:<36}".format("-"*12, "-"*13, "-"*13, "-"*7, "-"*36))
+    for i in range(len(machine_id)):
+        for j in range(machine_id[i][1]):
+            if j == 0:
+                print("   {:<12}\t{:<13}\t{:<13}\t{:<7}\t{:<36}".format(machine_id[i][0], snapshot_data[machine_id[i][0]][j][0], snapshot_data[machine_id[i][0]][j][1], snapshot_data[machine_id[i][0]][j][2], snapshot_data[machine_id[i][0]][j][3]))
+            else:
+                print(" "*5 + "{:<3}\t{:<13}\t{:<13}\t{:<7}\t{:<36}".format("└─────────", snapshot_data[machine_id[i][0]][j][0], snapshot_data[machine_id[i][0]][j][1], snapshot_data[machine_id[i][0]][j][2], snapshot_data[machine_id[i][0]][j][3]))
+    
+def main():
+    api_session = {"vmware-api-session-id": get_api_session(base_url, vcenter_username, vcenter_password)}
+    machine_list = get_machine_list(base_url, api_session)
     pre_vsphere_ui_jsessionid = pre_auth(base_url, headers)
     location_302 = redirect(base_url, pre_vsphere_ui_jsessionid, headers)
     result, castle_session = login(location_302, vcenter_username, vcenter_password, pre_vsphere_ui_jsessionid, headers)
     vsphere_username, vsphere_client_session_index, vsphere_ui_jsessionid = multi_cookies(result, castle_session, pre_vsphere_ui_jsessionid, headers)
     vsphere_ui_xsrf_token = xsrf_token(base_url, vsphere_username, vsphere_client_session_index, vsphere_ui_jsessionid, castle_session, headers)
     cookies = ret_cookies(vsphere_ui_jsessionid, vsphere_username, vsphere_client_session_index, vsphere_ui_xsrf_token, castle_session)
+    snapshot_data = get_snapshot_id(base_url, machine_list, cookies)
     while True:
         print("--------------------------------------------")
-        print("[*] Available Machine : DC01 DC02 WS01 WS02")
-        machine = input("[?] Select Revert Machine : ")
-        revert(machine, base_url, cookies, headers)
+        show_snapshots(snapshot_data)
+        print("[*] Please enter the snapshot ID to revert machine.")
+        machine = input("[?] Snapshot ID : ")
+        revert(machine, snapshot_data, base_url, cookies, headers)
 
-
-main(base_url, vcenter_username, vcenter_password, headers)
+main()
